aa5f139d5ca7323eb37f13bc2c5a448c0779b7f2
mikch06 <tools@kissel.ch>
2019-02-01 15:47:04 +0100

    revised script in different blocks
    - new order
    - work -> tmp

--- unbound-adb-update-hosts.sh
+++ unbound-adb-update-hosts.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-## Clean up any stale tempfile
-echo "Removing old files..."
-[ -f /tmp/hosts.working ] && rm -f /tmp/hosts.working
-
 ## Count entries in old blacklist
 oldlines=$(cat /tmp/ad-blacklist.conf|wc -l)
 
-## All Whitelists
+## Clean up any stale tempfile
+echo "Removing old files..."
+[ -f /tmp/hosts.tmp ] && rm -f /tmp/hosts.tmp
+
+## Awk regex to be inverse-matched as whitelist
+# SolveMedia is needed for captchas on some websites
 whitelist='/(api.solvemedia.com)/'
 
-# All ad, tracking and blacklists:
+# All Blacklists
 blacklist='
 http://winhelp2002.mvps.org/hosts.txt
 http://pgl.yoyo.org/as/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext
@@ -26,13 +27,22 @@ https://hosts-file.net/ad_servers.txt
 ## Fetch all Blacklist Files
 echo "Fetching Blacklists..."
 for url in $blacklist; do
-    curl --silent $url >> "/tmp/hosts.working"
+    curl --silent $url >> "/tmp/hosts.tmp"
 done
 
 ## Process Blacklist, Eliminiating Duplicates, Integrating Whitelist, and Converting to unbound format
 echo "Processing Blacklist..."
-awk -v whitelist="$whitelist" '$1 ~ /^127\.|^0\./ && $2 !~ whitelist {gsub("\r",""); print tolower($2)}' /tmp/hosts.working | sort | uniq | \
-awk '{printf "server:\n", $1; printf "local-data: \"%s A 0.0.0.0\"\n", $1}' > /var/unbound/ad-blacklist.conf
+awk -v whitelist="$whitelist" '$1 ~ /^127\.|^0\./ && $2 !~ whitelist {gsub("\r",""); print tolower($2)}' /tmp/hosts.tmp | sort | uniq | \
+awk '{printf "server:\n", $1; printf "local-data: \"%s A 0.0.0.0\"\n", $1}' > /tmp/ad-blacklist.conf
+
+## Count entries in new blacklist
+newlines=$(cat /tmp/ad-blacklist.conf|wc -l)
+
+## Clean up tempfile
+echo "Cleaning Up..."
+rm -f '/tmp/hosts.tmp'
+echo "Done. Please Restart the DNS Resolver service from the WebUI."
+echo
 
 ## Count entries in new blacklist
 newlines=$(cat /tmp/ad-blacklist.conf|wc -l)
@@ -42,8 +52,3 @@ echo "Blacklist entries before:"
 echo $oldlines
 echo "Blacklist entries now:"
 echo $newlines
-
-# Clean up tempfile
-echo "Cleaning Up..."
-rm -f '/tmp/hosts.working'
-echo "Done. Please Restart the DNS Resolver service from the WebUI."
