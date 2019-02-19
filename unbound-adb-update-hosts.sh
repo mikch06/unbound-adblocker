b3564483b52396c2fb411d25b2458f8404ff10b8
mikch06 <tools@kissel.ch>
2019-02-19 18:25:46 +0100

    fix typo in hostlist

--- unbound-adb-update-hosts.sh
+++ unbound-adb-update-hosts.sh
@@ -21,7 +21,8 @@ http://pgl.yoyo.org/as/serverlist.php?hostformat=hosts&showintro=0&mimetype=plai
 https://adaway.org/hosts.txt
 https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 https://mirror1.malwaredomains.com/files/justdomains
-http://sysctl.org/cameleon/hosts https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist
+http://sysctl.org/cameleon/hosts 
+https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist
 https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt
 https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt
 https://hosts-file.net/ad_servers.txt
