87ed8d0710d1385979bc8e7eeb1f545e5088f3b3
root <root@interno.kissel.home>
2019-02-20 23:27:21 +0100

    new running version, sorts uniq and fine, an all entries. open:  - correct paths  - remove comments

--- unbound-adb-update-hosts.sh
+++ unbound-adb-update-hosts.sh
@@ -4,14 +4,14 @@
 # unbound entries will be sorted and uniqe in the ad-blacklist.conf
 # file in your /var/unbound folder.
 
-## Count entries in old blacklist
+# Count entries in old blacklist
 oldlines=$(cat /var/unbound/ad-blacklist.conf|wc -l)
 
 ## Backup ad-blacklist.conf
 cp /var/unbound/ad-blacklist.conf /var/unbound/ad-blacklist.conf.bak
 
-## Clean up any stale tempfile
-##echo "Removing old files..."
+# Clean up any stale tempfile
+echo "Removing old files..."
 ##[ -f /tmp/hosts.tmp ] && rm -f /tmp/hosts.tmp
 
 # Ads- and Blacklists
@@ -27,7 +27,7 @@ https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt
 https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt
 https://hosts-file.net/ad_servers.txt
 '
-## Fetch all Blacklist Files
+# Fetch all Blacklist Files
 echo "Fetching Blacklists..."
 for url in $blacklist; do
     curl --silent $url >> "/tmp/hosts-download.tmp"
@@ -36,28 +36,26 @@ done
 
 # Process Blacklists, filter IP and alphabetical entries.
 echo "Processing Blacklist..."
-##time cat /tmp/hosts.tmp|grep '^127.0\|^0.0\|awk {gsub("\r","")};{print $2}'|sort|uniq -ui| \
-##awk '{printf "local-data: \"%s A 0.0.0.0\"\n", $1}' > /var/unbound/ad-blacklist.conf
 
-cat /tmp/hosts-download.tmp|grep '^127.0\|^0.0.0.0'|awk '{print $2}' > hosts-raw.tmp
-cat /tmp/hosts-download.tmp|grep '^[a-z]'|awk '{print $1}' >> hosts-raw.tmp
+cat /tmp/hosts-download.tmp|grep '^127.0\|^0.0.0.0'|awk '{print $2}' > /tmp/hosts-raw.tmp
+cat /tmp/hosts-download.tmp|grep '^[a-z]'|awk '{print $1}' >> /tmp/hosts-raw.tmp
 
 # Combine ad-blacklist, remove duplicates, converting to unbound format
-echo "Combine ad-blacklist"
-cat /tmp/hosts-raw.tmp|sort|uniq|awk '{printf "local-data: \"%sA 0.0.0.0\"\n", $1}' > /tmp/ad-blacklist.conf
+echo "Combine ad-blacklist..."
+cat /tmp/hosts-raw.tmp|awk '{gsub("\r","")};{print "local-data: " "\"" $0" A 0.0.0.0\""}'|sort|uniq > ad-blacklist.conf
 
-## Count entries in new blacklist
+# Count entries in new blacklist
 newlines=$(cat /var/unbound/ad-blacklist.conf|wc -l)
 
-## Clean up tempfile
+# Clean up tempfile
 echo "Cleaning Up..."
 ##rm -f '/tmp/hosts.tmp'
 echo
 
-## Count entries in new blacklist
+# Count entries in new blacklist
 newlines=$(cat /var/unbound/ad-blacklist.conf|wc -l)
 
-## Diff entries
+# Diff entries
 echo "Blacklist entries before:"
 echo $oldlines
 echo "Blacklist entries now:"
